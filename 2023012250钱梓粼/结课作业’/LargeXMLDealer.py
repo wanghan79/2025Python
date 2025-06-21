9409a7ca63cced5fd52a6244929d1dbcbcb48ebc
qianmimidy <2446518721@qq.com>
2025-06-21 20:58:23 +0800

    Update LargeXMLDealer.py

--- 2023012250钱梓粼/结课作业’/LargeXMLDealer.py
+++ 2023012250钱梓粼/结课作业’/LargeXMLDealer.py
@@ -1,83 +1,52 @@
-import xml.etree.ElementTree as ET
-from functools import wraps
+from lxml import etree
+from os import path
 
-def xml_parser_decorator(func):
+def xml_parse_decorator(fileName, elemTag):
     """
-    用于解析 XML文件的装饰器
-    处理XML解析并将提取的数据传递给目标函数
+    装饰器：解析大XML文件，将每个elemTag元素传递给被修饰函数
     """
-    @wraps(func)
-    def wrapper(file_path, *args, **kwargs):
-        # 定义命名空间
-        ns = {'uniprot': 'http://uniprot.org/uniprot'}
-        
-        # 使用iterparse进行迭代解析，避免内存问题
-        extracted_data = []
-        for event, elem in ET.iterparse(file_path, events=('end',)):
-            if elem.tag == f'{{{ns["uniprot"]}}}entry':
-                data = {}
-                
-                # 提取accession列表
-                data['accessions'] = [acc.text for acc in elem.findall('uniprot:accession', ns)]
-                
-                # 提取蛋白质名称
-                name_elem = elem.find('uniprot:name', ns)
-                data['name'] = name_elem.text if name_elem is not None else "N/A"
-                
-                # 提取蛋白质描述
-                protein = {}
-                protein_elem = elem.find('uniprot:protein', ns)
-                if protein_elem:
-                    rec_name = protein_elem.find('uniprot:recommendedName/uniprot:fullName', ns)
-                    protein['recommendedName'] = rec_name.text if rec_name else "N/A"
-                    
-                    alt_names = [alt.text for alt in protein_elem.findall('uniprot:alternativeName/uniprot:fullName', ns)]
-                    protein['alternativeNames'] = alt_names
-                
-                data['protein'] = protein
-                
-                # 提取基因信息
-                gene_elem = elem.find('uniprot:gene/uniprot:name[@type="primary"]', ns)
-                data['gene'] = gene_elem.text if gene_elem else "N/A"
-                
-                # 提取生物体信息
-                organism = {}
-                org_elem = elem.find('uniprot:organism', ns)
-                if org_elem:
-                    organism['scientific'] = org_elem.find('uniprot:name[@type="scientific"]', ns).text
-                    organism['common'] = org_elem.find('uniprot:name[@type="common"]', ns).text
-                    organism['taxonomy'] = org_elem.find('uniprot:dbReference[@type="NCBI Taxonomy"]', ns).attrib.get('id', 'N/A')
-                
-                data['organism'] = organism
-                
-                # 提取参考文献
-                references = []
-                for ref in elem.findall('uniprot:reference', ns):
-                    ref_data = {}
-                    citation = ref.find('uniprot:citation', ns)
-                    if citation:
-                        ref_data['type'] = citation.get('type', 'N/A')
-                        ref_data['title'] = citation.find('uniprot:title', ns).text if citation.find('uniprot:title', ns) is not None else "N/A"
-                        
-                        # 处理作者列表
-                        authors = []
-                        author_list = citation.find('uniprot:authorList', ns)
-                        if author_list:
-                            authors = [author.get('name') for author in author_list.findall('uniprot:person', ns)]
-                        ref_data['authors'] = authors
-                    
-                    references.append(ref_data)
-                
-                data['references'] = references
-                extracted_data.append(data)
-                
-                # 清理元素以节省内存
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if not path.isfile(fileName) or not fileName.endswith("xml"):
+                raise FileNotFoundError(f"{fileName} 不存在或不是xml文件")
+            count = 0
+            ns = _getNamespace(fileName)
+            ns = "{%s}" % ns if ns else ""
+            context = etree.iterparse(fileName, events=('end',), tag=ns + elemTag)
+            for event, elem in context:
+                func(elem, *args, **kwargs)
                 elem.clear()
-        
-        # 将解析后的数据传递给目标函数
-        return func(extracted_data, *args, **kwargs)
-    
-    return wrapper
+                count += 1
+                while elem.getprevious() is not None:
+                    del elem.getparent()[0]
+            del context
+            print(f"Already parsed {count} XML elements.")
+            return count
+        return wrapper
+    return decorator
+
+def _getNamespace(fileName):
+    if not path.isfile(fileName) or not fileName.endswith("xml"):
+        raise FileNotFoundError
+    result = ''
+    context = etree.iterparse(fileName, events=('start-ns',))
+    for event, elem in context:
+        prefix, result = elem
+        break
+    del context
+    return result
+    import sys
+from largeXMLDealer10 import xml_parse_decorator
+
 if __name__ == "__main__":
-    xml_file = "P00734.xml"  
-    print_protein_data(xml_file)
+    if len(sys.argv) != 3:
+        print("Usage: python callDealer11.py <xml_file> <element_tag>")
+        sys.exit(1)
+    fileName = sys.argv[1]
+    elemTag = sys.argv[2]
+
+    @xml_parse_decorator(fileName, elemTag)
+    def print_elem(elem):
+        print(elem.text.strip() if elem.text else "")
+
+    print_elem()  
