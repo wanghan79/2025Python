0626553b3ea8ab015ad1c01c0d254e52f18c9d56
zsq <149917739+zsq-zsq-zsq@users.noreply.github.com>
2025-06-29 00:43:55 +0800

    Create largeXMLDealer.py

---
+++ 2023012248张绍启/结课作业/largeXMLDealer.py
@@ -0,0 +1,53 @@
+
+
+from lxml import etree
+from os import path
+from functools import wraps
+
+
+def parse_large_xml(fileName, elemTag):
+
+    if not path.isfile(fileName) or not fileName.endswith("xml"):
+        raise FileNotFoundError(f"The file {fileName} does not exist or is not an XML file.")
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            count = 0
+            ns = _get_namespace(fileName)
+            namespace_tag = f"{{{ns}}}{elemTag}" if ns else elemTag
+
+            context = etree.iterparse(fileName, events=('end',), tag=namespace_tag)
+
+            for event, elem in context:
+                try:
+                    func(elem, *args, **kwargs)
+                except Exception as e:
+                    raise RuntimeError(f"Error calling user-defined function on element: {e}")
+                finally:
+                    elem.clear()
+                    count += 1
+                    while elem.getprevious() is not None:
+                        del elem.getparent()[0]
+            del context
+            return count
+
+        return wrapper
+
+    return decorator
+
+
+def _get_namespace(fileName):
+    if not path.isfile(fileName) or not fileName.endswith("xml"):
+        raise FileNotFoundError(f"The file {fileName} does not exist or is not an XML file.")
+
+    result = ''
+    context = etree.iterparse(fileName, events=('start-ns',))
+
+    for event, elem in context:
+        prefix, uri = elem
+        result = uri
+        break
+
+    del context
+    return result
